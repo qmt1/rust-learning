457a4eab77981c2084f2b37ff9c36f34f7f4eb52
QMT <steamuhq@gmail.com>
2024-10-13 16:34:38 +0200

    Add files via upload

---
+++ target/debug/deps/first.d
@@ -0,0 +1,5 @@
+C:\Users\nadek\Desktop\prj\learning rust\first\target\debug\deps\first.exe: src/main.rs
+
+C:\Users\nadek\Desktop\prj\learning rust\first\target\debug\deps\first.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/first.d
@@ -0,0 +1,1 @@
+C:\Users\nadek\Desktop\prj\learning\ rust\first\target\debug\first.exe: C:\Users\nadek\Desktop\prj\learning\ rust\first\src\main.rs
